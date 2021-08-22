9a69ba7a9ab5fb19bb12fe05c27ebe8d6f508c71
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-08-22 16:14:15 -0300

    chore: typeorm config

---
+++ ormconfig.ts
@@ -0,0 +1,13 @@
+import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
+
+const config: SqliteConnectionOptions = {
+  type: 'sqlite',
+  database: 'db',
+  entities: ['dist/src/**/*.entity.js'],
+  synchronize: true, // not for production!
+  migrations: ['dist/src/db/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/db/migrations',
+  },
+};
+export default config;
