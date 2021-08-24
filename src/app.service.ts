54173d31d51728bf5fe961578a5355e283ac61c1
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2021-08-23 23:26:49 -0300

    feat: seed db data

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
+import { ContactInfo } from './contact-info.entity';
+import { Employee } from './employee.entity';
+import { Meeting } from './meeting.entity';
+import { Task } from './task.entity';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello Worldcito';
+  constructor(
+    @InjectRepository(Employee)
+    private employeeRepo: Repository<Employee>,
+    @InjectRepository(ContactInfo)
+    private contactInfoRepo: Repository<ContactInfo>,
+    @InjectRepository(Meeting) private meetingRepo: Repository<Meeting>,
+    @InjectRepository(Task) private taskRepo: Repository<Task>,
+  ) {}
+
+  async seed(): Promise<void> {
+    const ceo = this.employeeRepo.create({ name: 'Full CEO Crack' });
+    await this.employeeRepo.save(ceo);
+
+    const ceoContactInfo = this.contactInfoRepo.create({
+      email: 'email@email.com',
+    });
+
+    ceoContactInfo.employee = ceo;
+    await this.contactInfoRepo.save(ceoContactInfo);
+
+    const manager = this.employeeRepo.create({ name: 'Luis', manager: ceo });
+
+    const task1 = this.taskRepo.create({ name: 'Hire argentine workers' });
+    await this.taskRepo.save(task1);
+
+    const task2 = this.taskRepo.create({ name: 'Present to CEO' });
+    await this.taskRepo.save(task2);
+
+    manager.tasks = [task1, task2];
+
+    const meeting1 = this.meetingRepo.create({ zoomUrl: 'meeting.com' });
+    meeting1.attendees = [ceo];
+    await this.meetingRepo.save(meeting1);
+    manager.meetings = [meeting1];
+
+    await this.employeeRepo.save(manager);
   }
 }
--- src/meeting.entity.ts
+++ src/meeting.entity.ts
@@ -1,6 +1,7 @@
-import { Column, ManyToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { Employee } from './employee.entity';
 
+@Entity()
 export class Meeting {
   @PrimaryGeneratedColumn()
   id: number;
